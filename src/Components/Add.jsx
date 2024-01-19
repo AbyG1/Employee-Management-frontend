2535b817ea40bb69ef84d8effa9193e6e1c0eb40
AbyG1 <abygeorge133@gmail.com>
2024-01-19 20:42:34 +0530

    Implement data add functionality

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { MDBInput } from 'mdb-react-ui-kit';
-
+import axios from 'axios'
 
 
 function Add() {
@@ -10,8 +10,27 @@ function Add() {
   const [age,setAge] = useState("")
   const [designation, setDesignation] = useState("")
   const [salary, setSalary] = useState("")
+  
+  
+
+  const base_url='http://localhost:8000/add-an-employee'
+  const addEmployee = async(e) => {
+     e.preventDefault()
+    //add employee - api call
+    console.log(id,name,age,designation,salary)
+  
 
   
+    //api call to add employee details to the mongodb
+    const body = {id,name,age,designation,salary}
+    console.log('Before Axios request');
+    const result = await axios.post(base_url,body)
+    console.log('After Axios request', result.data);
+  } 
+
+
+
+
   return (
 
      
@@ -26,13 +45,13 @@ function Add() {
 
       <form action="emp-form" className='p-5'>
 
-      <MDBInput label='ID' id='form1' type='text' className='my-2'/>
-      <MDBInput label='Name' id='form1' type='text' className='my-2'/>
-      <MDBInput label='Age' id='form1' type='text' className='my-2'/>
-      <MDBInput label='Designation' id='form1' type='text' className='my-2' />
-      <MDBInput label='Salary' id='form1' type='text' className='my-2' />
+      <MDBInput onChange={(e) => setId(e.target.value)} label='ID' id='form1' type='text' className='my-2'/>
+      <MDBInput onChange={(e) =>setName(e.target.value)} label='Name' id='form1' type='text' className='my-2'/>
+      <MDBInput onChange={(e) => setAge(e.target.value)} label='Age' id='form1' type='text' className='my-2'/>
+      <MDBInput onChange={(e) => setDesignation(e.target.value)}label='Designation' id='form1' type='text' className='my-2' />
+      <MDBInput onChange={(e) => setSalary(e.target.value)} label='Salary' id='form1' type='text' className='my-2' />
 
-      <button class='btn btn-success'>Add <i className='fa-solid fa-user-plus'></i></button>
+      <button className='btn btn-success' onClick={(e) => addEmployee(e)}>Add <i className='fa-solid fa-user-plus'></i></button>
       </form>
     
 
